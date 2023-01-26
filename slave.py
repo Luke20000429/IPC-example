7548a46a9ba8114886b9f16567fe5269e42ddc9c
Luke20000429 <liuxs@umich.edu>
2023-01-26 11:50:10 -0500

    add python side wrapper

--- slave.py
+++ slave.py
@@ -7,41 +7,53 @@ import os
 import time
 import numpy as np
 
-def main(r, w):
+class Wrapper:
+    
     path = "/tmp"
-    key = ipc.ftok(path, 2333, silence_warning=True)
-    shm = ipc.SharedMemory(key, 0, 0)
-
-    #I found if we do not attach ourselves
-    #it will attach as ReadOnly.
-    shm.attach(0,0) 
+    projID = 2333
     
-    while True:
-        readmessage = os.read(r, 8) # read a uint64 from pipe
-        data_size = int.from_bytes(readmessage, byteorder='little', signed=False)
-        print("[Python] Receive data size from master: ", data_size)
-        if data_size == 0:
-            break
+    def __init__(self, _r: int, _w: int):
+        self.r = _r
+        self.w = _w
         
-        buf = shm.read(data_size) # read 128*4 fmat from shared memory
-        # print("[Python] Data read from shared memory: ", buf)
-        pcd_size = int(data_size / 4 / 4) # only for float number 
-        fmat = np.frombuffer(buf, dtype='float32').reshape(pcd_size, 4)
-        # print("[Python] Convert bytes to array\n", fmat)
-        flot = fmat + 10
-        print("[Python] Add 10 to the matrix")
-        shm.write(flot.tobytes())
-        print("[Python] waiting for 1s...")
-        time.sleep(1) # do some process
-        os.write(w, b"Done")
-          
-    shm.detach()
-    
-    pass
+        self.key = ipc.ftok(self.path, self.projID, silence_warning=True)
+        self.shm = ipc.SharedMemory(self.key, 0, 0)
+
+        #I found if we do not attach ourselves
+        #it will attach as ReadOnly.
+        self.shm.attach(0,0) 
+        
+    def __call__(self, _func):
+        # output_data __func(input_buffer, n_bytes)
+        while True:
+            readmessage = os.read(self.r, 8) # read a uint64 from pipe
+            data_size = int.from_bytes(readmessage, byteorder='little', signed=False)
+            print("[Python] Receive data size from master: ", data_size)
+            if data_size == 0:
+                print("[Python] Exit!")
+                break
+            ibuf = self.shm.read(data_size) # read fmat from shared memory
+            # print("[Python] Data read from shared memory: ", buf)
+            obuf = _func(ibuf, data_size) # call user defined function
+            self.shm.write(obuf.tobytes())
+            print("[Python] waiting for 1s...")
+            time.sleep(1) # do some process
+            os.write(self.w, b"Done")
+        
+    def __del__(self):
+        self.shm.detach()
+        print("[Python] Detach from shared memory")
+
+def FLOT(input_buffer, n_bytes):
+    pcd_size = int(n_bytes / 4 / 4) # only for float number 
+    mat = np.frombuffer(input_buffer, dtype='float32').reshape(pcd_size, 4)
+    print("[Python] Add 10 to the matrix")
+    return mat + 10    
 
 if __name__ == '__main__':
     pipe1 = int(sys.argv[1])
     pipe2 = int(sys.argv[2])
+    wrap = Wrapper(pipe1, pipe2)
     print("[Python] read from pipe1: {}, write to pipe2: {}".format(pipe1, pipe2))
-    main(pipe1, pipe2)
+    wrap(FLOT)
     
